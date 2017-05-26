ef9d8cd0046d258c657808e6b2c999aac559aa16
ImKyd <kralikmatej@seznam.cz>
2017-05-26 20:40:39 +0200

    Update Main.php

--- src/AS/Main.php
+++ src/AS/Main.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace AS;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+
+class Main extends PluginBase{
+  
+  public $break;
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    $this->getLogger()->info("§aEnabling ByteAnticheat §e[Version 1.0]");
+  }
+  
+  public function addWarning(Player $p){
+    //TODO in next version
+  }
+  
+  public static function getPrefix(){
+    return "§l§dByteAC §r";
+  }
+}
